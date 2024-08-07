4e1a1f91a934c1e4d9ce86a5d4dc20e2de96fbf9
Roman Vytak <r.vytak@dynatech.lv>
2024-08-08 00:01:34 +0300

    updates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,11 @@
 
 export const isValidEmail = (email: string) => {
   if (!email) return false
-  return /\S+@\S+\.\S+/.test(email);
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
 
 export const isValidNumber = (number: string) => {
